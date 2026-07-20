35a709e636e674def155419e26c5f3d8f222bf83
unknown <parthamolsurve@gmail.com>
2026-07-21 00:42:23 +0530

    bug fixes2

--- crime-platform/frontend/next.config.ts
+++ crime-platform/frontend/next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/app",
+  images: {
+    unoptimized: true,
+  },
   devIndicators: false,
 };
 
